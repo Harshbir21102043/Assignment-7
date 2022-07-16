33dd58e0c064d0719f16324c22cb1241689c81cf
Harshbir21102043 <104373657+Harshbir21102043@users.noreply.github.com>
2022-07-16 20:29:43 +0530

    Add files via upload

---
+++ 7th - output.py
@@ -0,0 +1,34 @@
+Python 3.10.1 (tags/v3.10.1:2cd268a, Dec  6 2021, 19:10:37) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+
+= RESTART: C:/Users/chann/AppData/Local/Programs/Python/Python310/Assignment 7.py
+Enter size of list: 10
+Enter marks: 24
+Enter marks: 57
+Enter marks: 63
+Enter marks: 91
+Enter marks: 88
+Enter marks: 74
+Enter marks: 79
+Enter marks: 31
+Enter marks: 49
+Enter marks: 54
+[24, 31, 49, 54, 57, 63, 74, 79, 88, 91]
+Enter size of array: 6
+Enter element 1
+Enter element 2
+Enter element 3
+Enter element 4
+Enter element 5
+Enter element 6
+[1, 2, 3, 4, 5, 6]
+Enter element to be searched and counted: 4
+Element is present at index 3
+Number of occurrences of element is 1
+Enter number of inputs:5
+Enter number:1
+Enter number:2
+Enter number:3
+Enter number:2
+Enter number:3
+[1, 2, 3]
